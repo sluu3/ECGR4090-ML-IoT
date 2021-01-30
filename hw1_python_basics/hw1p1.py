0e829ce6c882984d697bb1a64cac7d9636ddd60e
Sam <sluu3@uncc.edu>
2021-01-29 23:10:17 -0500

    hw1

--- hw1_python_basics/hw1p1.py
+++ hw1_python_basics/hw1p1.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+import person
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 list_of_names = ['Roger', 'Mary', 'Luisa', 'Elvis']
 list_of_ages  = [23, 24, 19, 86]
@@ -7,4 +11,26 @@ list_of_heights_cm = [175, 162, 178, 182]
 for name in list_of_names:
   print("The name {:} is {:} letters long".format(name, len(name)))
   
+name_size = [len(x) for x in list_of_names]
 
+people = {}
+
+for x in range(len(list_of_names)):
+  new_person = person.person(name=list_of_names[x], age=list_of_ages[x], height=list_of_heights_cm[x])
+  people.update({list_of_names[x] : new_person})
+
+print(people)
+
+age_array = np.array(list_of_ages)
+height_array = np.array(list_of_heights_cm)
+
+avg_age = np.mean(age_array)
+print(f"The average age is {avg_age}")
+
+plt.scatter(list_of_ages, list_of_heights_cm, color='b')
+plt.grid(True)
+plt.xlabel('Age (yrs)')
+plt.ylabel('Height (cm)')
+plt.title('Height vs Age')
+plt.savefig('plot.png')
+plt.show()
